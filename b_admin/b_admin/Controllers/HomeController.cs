07e12dbc533accde77ca1ed2a26efb2d44e0b88c
fnaeijii <fatemehnaeiji2002@gmail.com>
2024-05-02 18:39:11 +0330

    upload baker_site project

---
+++ b_admin/b_admin/Controllers/HomeController.cs
@@ -0,0 +1,336 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using b_admin.Models;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Globalization;
+using System.Web.Security;
+using System.Threading;
+using System.Text;
+
+namespace b_admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        Naeiji1465_baker context=new Naeiji1465_baker();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult user_management()
+        {
+            return View();
+        }
+
+        public ActionResult orders()
+        {
+            return View();
+        }
+        public ActionResult dargah()
+        {
+            return View();
+        }
+        public ActionResult getcomm()
+        {
+            try
+            {
+                var res = context.tbl_Comm.Where(x => x.Typee != "Q").Select(x => new { x.pkID, x.Name, x.Valuee, x.Typee }).ToList();
+                return Json(new { state = true, m = res }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception e)
+            {
+                return Json(new { state = false, m = e.Message }, JsonRequestBehavior.AllowGet);
+            }
+        }
+        public ActionResult getuser()
+        {
+            try
+            {
+                var res = context.tbl_User.Select(x => new { x.pkID, x.Name, x.Phone, x.Status }).ToList();
+                return Json(new { state = true, m = res }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception e)
+            {
+                return Json(new { state = false, m = e.Message }, JsonRequestBehavior.AllowGet);
+            }
+        }
+        public ActionResult getorders()
+        {
+            try
+            {
+                var res = context.tbl_order.Where(x=>x.pay==true).Select(x => new { x.pkID, x.number, x.fkUserID, x.pay,x.time,x.dis ,x.send,x.factornumber}).ToList();
+                
+                return Json(new { state = true, m = res }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception e)
+            {
+                return Json(new { state = false, m = e.Message }, JsonRequestBehavior.AllowGet);
+            }
+        }
+        public  ActionResult getU(int id)
+        {
+            var res=context.tbl_User.Where(x=>x.pkID==id).Select(x => new { x.pkID, x.Name, x.Phone, x.Status,x.address }).ToList();
+            return Json( res , JsonRequestBehavior.AllowGet);
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult commtblch(int id, string valuee)
+        {
+            try
+            {
+                var res = context.tbl_Comm.Where(x => x.pkID == id).Single();
+                res.Valuee = valuee;
+                context.SaveChanges();
+                return Json(new { state = true, m = valuee }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception e)
+            {
+                return Json(new { state = false, m = e.Message }, JsonRequestBehavior.AllowGet);
+            }
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult usertblch(int id, string valuee)
+        {
+            try
+            {
+                var res = context.tbl_User.Where(x => x.pkID == id).Single();
+                res.Status = valuee;
+                context.SaveChanges();
+                return Json(new { state = true, m = valuee }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception e)
+            {
+                return Json(new { state = false, m = e.Message }, JsonRequestBehavior.AllowGet);
+            }
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult chgimg(int id)
+
+        {
+
+            try
+            {
+
+
+
+
+                if (System.Web.HttpContext.Current.Request.Files.AllKeys.Any())
+                {
+                    var file = System.Web.HttpContext.Current.Request.Files["image"];
+
+
+                    if (file.ContentLength > 3000000) { return Json(new { state = false, m = "حجم عکس باید کوچکتر از 3 مگا بایت باشد" }, JsonRequestBehavior.AllowGet); }
+
+                    FtpWebRequest ftpRequest =
+                   (FtpWebRequest)WebRequest.Create("ftp://irwrs1.dnswebhost.com/httpdocs/baker_img");
+
+
+
+                    ftpRequest.Credentials = new NetworkCredential("sysuser_2b0", "$vpjjn1T8y0ZDbl&");
+                    ftpRequest.Method = WebRequestMethods.Ftp.ListDirectory;
+                    ftpRequest.EnableSsl = true;
+                    FtpWebResponse response = (FtpWebResponse)ftpRequest.GetResponse();
+                    StreamReader streamReader = new StreamReader(response.GetResponseStream());
+
+
+
+                    string imgname = "_" + id + "_";
+                    string pic = "";
+                    string line = streamReader.ReadLine();
+                    while (!string.IsNullOrEmpty(line))
+                    {
+                        if (line.Contains(imgname))
+                        {
+                            pic = line;
+                            line = streamReader.ReadLine();
+
+
+                            break;
+                        }
+                        else
+                        {
+                            line = streamReader.ReadLine();
+                        }
+                    }
+
+                    streamReader.Close();
+
+                    if (pic != "")
+                    {
+                        FtpWebRequest request = (FtpWebRequest)WebRequest.Create("ftp://irwrs1.dnswebhost.com/httpdocs/baker_img/" + pic);
+                        request.Method = WebRequestMethods.Ftp.DeleteFile;
+                        request.Credentials = new NetworkCredential("sysuser_2b0", "$vpjjn1T8y0ZDbl&");
+
+                        using (FtpWebResponse response2 = (FtpWebResponse)request.GetResponse())
+                        {
+                            pic = response2.StatusDescription;
+                        }
+
+                    }
+
+
+
+
+
+                    string extention = Path.GetExtension(file.FileName).ToLower();
+
+                    var uploadurl = "ftp://irwrs1.dnswebhost.com/httpdocs/baker_img";
+                    var uploadfilename = "_" + id + "_(" + DateTime.Now + ")" + extention;
+                    uploadfilename = uploadfilename.Replace(@"/", "-");
+                    uploadfilename = uploadfilename.Replace(@":", "-");
+                    var username = "sysuser_2b0";
+                    var password = "$vpjjn1T8y0ZDbl&";
+                    Stream streamObj = file.InputStream;
+                    byte[] buffer = new byte[file.ContentLength];
+                    streamObj.Read(buffer, 0, buffer.Length);
+                    streamObj.Close();
+                    streamObj = null;
+                    string ftpurl = String.Format("{0}/{1}", uploadurl, uploadfilename);
+                    var requestObj = FtpWebRequest.Create(ftpurl) as FtpWebRequest;
+
+                    requestObj.Method = WebRequestMethods.Ftp.UploadFile;
+                    requestObj.Credentials = new NetworkCredential(username, password);
+
+                    Stream requestStream = requestObj.GetRequestStream();
+
+                    requestStream.Write(buffer, 0, buffer.Length);
+                    requestStream.Flush();
+                    requestStream.Close();
+                    requestObj = null;
+
+
+
+
+
+                    var res = context.tbl_Comm.Where(x => x.pkID == id).Single();
+                    res.Valuee = "http://infinite20.xyz/baker_img/" + uploadfilename;
+
+
+                    context.SaveChanges();
+
+
+                    return Json(new { state = true, nimg = uploadfilename, m = "عکس با موفقیت بارگزاری شد" }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    return Json(new { state = false, m = "عکس یافت نشد" }, JsonRequestBehavior.AllowGet);
+                }
+
+
+            }
+            catch (Exception e)
+            {
+                return Json(new { state = false, m = e.Message }, JsonRequestBehavior.AllowGet);
+            }
+
+        }
+        public void send(bool state)
+        {
+            try
+            {
+                if (state)
+                {
+                    var res = context.tbl_Comm.Where(x => x.Name == "ارسال").SingleOrDefault();
+                    res.Valuee = "true";
+                    context.SaveChanges();
+                }
+                else
+                {
+                    var res = context.tbl_Comm.Where(x => x.Name == "ارسال").SingleOrDefault();
+                    res.Valuee = "false";
+                    context.SaveChanges();
+                }
+            }catch(Exception e)
+            {
+
+            }
+        }
+        public ActionResult login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult user_login(string pn, string pas)
+        {
+            int status = 0;
+            var user = context.tbl_Owner.Where(x => x.usename == pn).SingleOrDefault();
+
+            if (user != null)
+            {
+                if (user.password == pas)
+                {
+                    
+                    var cookieText = Encoding.UTF8.GetBytes(user.pkID.ToString());
+                    var encryptedValue = Convert.ToBase64String(cookieText);
+
+
+
+                    Response.Cookies["enfinit@tryadmin"].Value = encryptedValue;
+                    Response.Cookies["enfinit@tryadmin"].Expires = DateTime.Now.AddDays(500);
+
+                    status = 1; //login ok
+                }
+                else
+                {
+                    status = 2; //wrong pass
+
+
+                }
+
+
+            }
+
+            else
+            {
+                status = 3; //dosent match pn
+
+            }
+
+
+            return Json(status, JsonRequestBehavior.AllowGet);
+        }
+
+        public void setdargah(string send,string result,string pin ,string callback)
+        {
+            var bytes = Convert.FromBase64String(Request.Cookies["enfinit@tryadmin"].Value);
+            string result1 = Encoding.UTF8.GetString(bytes);
+            int userid = int.Parse(result1);
+            var user = context.tbl_Owner.Where(x => x.pkID == userid).SingleOrDefault();
+
+            user.GatewaySend=send;
+            user.GatewayResult=result;
+            user.pin=pin;
+            user.callback=callback;
+            context.SaveChanges();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult removeorder(int vid)
+        {
+
+            
+            var visit = context.tbl_order.Where(x => x.pkID == vid).SingleOrDefault();
+
+            context.tbl_order.Remove(visit);
+            context.SaveChanges();
+            return Json(true, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ baker/baker/Controllers/HomeController.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using baker.Models;
+using System.Globalization;
+using System.Web.Security;
+using System.Net;
+
+namespace baker.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        Bdata context = new Bdata();
+
+        public ActionResult Index()
+        {
+            var res = context.tbl_Comm.ToList();
+            ViewBag.title = res.Where(x => x.Name == "تیتر اصلی").Select(x => x.Valuee).SingleOrDefault();
+
+            ViewBag.banerimg = res.Where(x => x.Name == "عکس بنر").Select(x => x.Valuee).SingleOrDefault();
+
+            ViewBag.address = res.Where(x => x.Name == "نشانی").Select(x => x.Valuee).SingleOrDefault();
+
+            ViewBag.phone = res.Where(x => x.Name == "شماره تماس").Select(x => x.Valuee).SingleOrDefault();
+
+            ViewBag.email = res.Where(x => x.Name == "ایمیل").Select(x => x.Valuee).SingleOrDefault();
+
+            ViewBag.dis = res.Where(x => x.Name == "توضیحات").Select(x => x.Valuee).SingleOrDefault();
+
+            ViewBag.Nname = res.Where(x => x.Name == "اسم نان").Select(x => x.Valuee).SingleOrDefault();
+
+            ViewBag.Nprice = res.Where(x => x.Name == "قیمت نان").Select(x => x.Valuee).SingleOrDefault();
+
+            ViewBag.Nimg = res.Where(x => x.Name == "تصویر نان").Select(x => x.Valuee).SingleOrDefault();
+
+            ViewBag.Ndis = res.Where(x => x.Name == "توضیح نان").Select(x => x.Valuee).SingleOrDefault();
+
+            ViewBag.Psend = res.Where(x => x.Name == "هزینه ارسال").Select(x => x.Valuee).SingleOrDefault();
+
+
+
+            ViewBag.sattime = res.Where(x => x.pkID == 12).Select(x => x.Valuee).SingleOrDefault();
+
+            ViewBag.suntime = res.Where(x => x.pkID == 13).Select(x => x.Valuee).SingleOrDefault();
+
+            ViewBag.montime = res.Where(x => x.pkID == 14).Select(x => x.Valuee).SingleOrDefault();
+
+            ViewBag.tuetime = res.Where(x => x.pkID == 15).Select(x => x.Valuee).SingleOrDefault();
+
+            ViewBag.wentime = res.Where(x => x.pkID == 16).Select(x => x.Valuee).SingleOrDefault();
+
+            ViewBag.thutime = res.Where(x => x.pkID == 17).Select(x => x.Valuee).SingleOrDefault();
+
+            ViewBag.fritime = res.Where(x => x.pkID == 18).Select(x => x.Valuee).SingleOrDefault();
+            return View();
+        }
+        public ActionResult login()
+        {
+            var res = context.tbl_Comm.ToList();
+          
+
+            ViewBag.banerimg = res.Where(x => x.Name == "عکس بنر").Select(x => x.Valuee).SingleOrDefault();
+
+            return View();
+        }
+        public ActionResult user_account()
+        {
+            var res = context.tbl_Comm.ToList();
+            ViewBag.title = res.Where(x => x.Name == "تیتر اصلی").Select(x => x.Valuee).SingleOrDefault();
+
+            ViewBag.banerimg = res.Where(x => x.Name == "عکس بنر").Select(x => x.Valuee).SingleOrDefault();
+
+            ViewBag.address = res.Where(x => x.Name == "نشانی").Select(x => x.Valuee).SingleOrDefault();
+
+            ViewBag.phone = res.Where(x => x.Name == "شماره تماس").Select(x => x.Valuee).SingleOrDefault();
+
+            ViewBag.email = res.Where(x => x.Name == "ایمیل").Select(x => x.Valuee).SingleOrDefault();
+
+            ViewBag.Psend = res.Where(x => x.Name == "*هزینه ارسال").Select(x => x.Valuee).SingleOrDefault();
+
+            ViewBag.send = res.Where(x => x.Name == "ارسال").Select(x => x.Valuee).SingleOrDefault();
+
+            if (Request.Cookies["enfinit@try1"] != null) { 
+            var bytes = Convert.FromBase64String(Request.Cookies["enfinit@try1"].Value);
+            string result = Encoding.UTF8.GetString(bytes);
+            int userid = int.Parse(result);
+            var user = context.tbl_User.Where(x => x.pkID == userid).SingleOrDefault();
+
+            ViewBag.name = user.Name;
+            ViewBag.inventory = user.inventory;
+            ViewBag.useraddress = user.address;
+            ViewBag.status = user.Status;
+        }
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult user_login(string pn, string pas)
+        {
+            //Session.Timeout = 30;
+            int status = 0;
+            var user = context.tbl_User.Where(x => x.Phone == pn).SingleOrDefault();
+
+            if (user != null)
+            {
+                if (user.Password == pas)
+                {
+                    //Session["userid"] = user.pkID;
+                    var cookieText = Encoding.UTF8.GetBytes(user.pkID.ToString());
+                    var encryptedValue = Convert.ToBase64String(cookieText);
+
+
+
+                    Response.Cookies["enfinit@try1"].Value = encryptedValue;
+                    Response.Cookies["enfinit@try1"].Expires = DateTime.Now.AddDays(500);
+                   
+                    status = 1; //login ok
+                }
+                else
+                {
+                    status = 2; //wrong pass
+
+
+                }
+
+
+            }
+
+            else
+            {
+                status = 3; //dosent match pn
+
+            }
+
+
+            return Json(status, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult user_signup(string namee, string phone,string address, string pass,string pass2)
+        {
+
+            int statee = 0;
+            if (namee == "" || phone == "" || pass == "" || pass2 == "")
+            {
+                statee = 3;
+            }
+            else
+            {
+                if (pass == pass2)
+                {
+
+
+                    var p = context.tbl_User.Where(x => x.Phone == phone).SingleOrDefault();
+                    if (p == null)
+                    {
+                        tbl_User newU = new tbl_User();
+                        newU.Name = namee;
+                        newU.Phone = phone;
+                        newU.address = address;
+                        newU.Password = pass;
+                        newU.Status = "عادی";
+                        context.tbl_User.Add(newU);
+                        context.SaveChanges();
+                        var cookieText = Encoding.UTF8.GetBytes(newU.pkID.ToString());
+                        var encryptedValue = Convert.ToBase64String(cookieText);
+
+
+
+                        Response.Cookies["enfinit@try1"].Value = encryptedValue;
+                        Response.Cookies["enfinit@try1"].Expires = DateTime.Now.AddDays(500);
+
+                        statee = 2;
+
+                    }
+                    else
+                    {
+                        statee = 1;
+                    }
+
+                    context.SaveChanges();
+
+                }
+            }
+            return Json(statee, JsonRequestBehavior.AllowGet);
+
+        }
+        public void logout()
+        {
+            Response.Cookies["enfinit@try1"].Expires = DateTime.Now.AddDays(-1);
+          
+            Response.Redirect("/Home/index");
+        }
+
+        public ActionResult checksend()
+        {
+            var res = context.tbl_Comm.Where(x => x.Name == "ارسال").Select(x => x.Valuee).SingleOrDefault();
+            var Psend = context.tbl_Comm.Where(x => x.Name == "*هزینه ارسال").Select(x => x.Valuee).SingleOrDefault();
+           
+                var bytes = Convert.FromBase64String(Request.Cookies["enfinit@try1"].Value);
+                string result = Encoding.UTF8.GetString(bytes);
+                int userid = int.Parse(result);
+                var user = context.tbl_User.Where(x => x.pkID == userid).SingleOrDefault();
+
+                return Json(new { Psend = Psend, useraddress = user.address, res = res }, JsonRequestBehavior.AllowGet);
+             
+        }
+        public ActionResult checksend2()
+        {
+            var res = context.tbl_Comm.Where(x => x.Name == "ارسال").Select(x => x.Valuee).SingleOrDefault();
+            var Psend = context.tbl_Comm.Where(x => x.Name == "*هزینه ارسال").Select(x => x.Valuee).SingleOrDefault();
+
+            return Json(new { Psend = Psend, res = res }, JsonRequestBehavior.AllowGet);
+
+
+        }
+
+        public void changeaddress(string address)
+        {
+           
+            var bytes = Convert.FromBase64String(Request.Cookies["enfinit@try1"].Value);
+            string result = Encoding.UTF8.GetString(bytes);
+            int userid = int.Parse(result);
+            var user = context.tbl_User.Where(x => x.pkID == userid).SingleOrDefault();
+
+            user.address = address;
+            context.SaveChanges();
+        }
+        public ActionResult getfactor(int factor,bool status)
+        {
+            var price= int.Parse(context.tbl_Comm.Where(x => x.Name == "قیمت نان").Select(x => x.Valuee).SingleOrDefault());
+          
+             price *=factor;
+            if (status == true)
+            {
+                var Psend = int.Parse(context.tbl_Comm.Where(x => x.Name == "*هزینه ارسال").Select(x => x.Valuee).SingleOrDefault());
+                price += Psend;
+            }
+           
+            return Json(price, JsonRequestBehavior.AllowGet);
+        }
+
+        public void inventory(int price)
+        {
+            var bytes = Convert.FromBase64String(Request.Cookies["enfinit@try1"].Value);
+            string result = Encoding.UTF8.GetString(bytes);
+            int userid = int.Parse(result);
+            var user = context.tbl_User.Where(x => x.pkID == userid).SingleOrDefault();
+
+
+            user.inventory += price;
+            context.SaveChanges();
+
+        }
+
+
+        public ActionResult invoice(int factor,string time,string dis,int price ,bool send)
+        {
+            var factornumber=0;
+            try
+            {
+                factornumber = context.tbl_order.Max(x => x.factornumber);
+               
+            }
+           catch(Exception e)
+            {
+                
+            }
+            factornumber++;
+            var bytes = Convert.FromBase64String(Request.Cookies["enfinit@try1"].Value);
+            string result = Encoding.UTF8.GetString(bytes);
+            int userid = int.Parse(result);
+            var user = context.tbl_User.Where(x => x.pkID == userid).SingleOrDefault();
+
+          
+
+            tbl_order ni = new tbl_order();
+            if (user.inventory >= price)
+            {
+                user.inventory -= price;
+                context.SaveChanges();
+                ni.pay = true;
+            }
+            else
+            {
+                ni.pay = false;
+            }
+                ni.fkUserID = userid;
+                ni.price = price;
+                ni.time = time;
+                ni.number = factor;
+                ni.send = send;
+                ni.transid = "0";
+                ni.trackingid = "0";
+                ni.factornumber = factornumber;
+            context.tbl_order.Add(ni);
+            context.SaveChanges();
+        
+          
+            return Json(new {user=user,pay=ni.pay,  fn = factornumber, price = price }, JsonRequestBehavior.AllowGet);
+
+        }
+      
+
+    }
+}
